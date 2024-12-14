5c2b5d11579a5f82205485bf6a83422843ab9932
Esteban Barragan <estebanbarragan@Estebans-MacBook-Pro.local>
2024-12-14 15:26:12 -0800

    feat(app): add cleanup function and reset component using keys

--- src/components/ProgressBar.jsx
+++ src/components/ProgressBar.jsx
@@ -2,22 +2,20 @@ import { useEffect, useState } from "react"
 
 export default function ProgressBar({ timeout, onTimeOut }) {
     const [remainingSeconds, setRemainingSeconds] = useState(timeout)
+    // This effect is to get to know when the timeout is reached
     useEffect(() => {
-      const timeoutId =  setTimeout(onTimeOut, timeout)
-      return ()=>{
-        clearTimeout(timeoutId)
-      }
-    }, [timeout])
+        const timeoutId = setTimeout(onTimeOut, timeout)
+        return () => clearTimeout(timeoutId)
+    }, [timeout, onTimeOut])
 
+    // This effect is to update the remaining seconds every 100ms
     useEffect(() => {
+        // Every 100ms, decrement the remainingSeconds by 100 until it reaches 0
         const interval = setInterval(() => {
-            setRemainingSeconds(prevRemainingSec => {
-                return prevRemainingSec - 100
-            })
-            return ()=>{
-                clearInterval(interval)
-            }
+            setRemainingSeconds(prevRemainingSec => prevRemainingSec - 100)
         }, 100)
+        return () => clearInterval(interval)
+
     }, [])
 
     return <progress id="question-time" max={timeout} value={remainingSeconds}></progress>
--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,17 +1,26 @@
-import { useState } from "react"
+import { useCallback, useState } from "react"
 import QuizCompletedImg from "../assets/quiz-complete.png"
 import QUESTIONS from "../questions"
 import ProgressBar from "./ProgressBar";
 
 export default function Quiz(){
     const [answers, setAnswers] = useState([])
+
     const currentActiveQuestion = answers.length;
+    // Check if the quiz is completed
     const isQuizCompleted = QUESTIONS.length === currentActiveQuestion
     function handleSelectAnswer(answer){
         setAnswers(prevAnswers=>{
             return [...prevAnswers,answer]
         })
     }
+    // There are multiple way to add comments in JavaScript
+    // This is a single line comment
+    /** 
+     * This is a multi-line comment
+     * It can span multiple lines
+     */ const handleSkipQuestion = useCallback(()=>handleSelectAnswer(null), [])
+
     if(isQuizCompleted){
         return <div id="summary">
             <img src={QuizCompletedImg} alt="Quiz completed image" />
@@ -24,7 +33,8 @@ export default function Quiz(){
     shuffledAnswers.sort(()=>Math.random() - 0.5)
     
     return <div id="quiz">
-    <ProgressBar timeout={10000} onTimeOut={()=>handleSelectAnswer(null)}/>
+    {/* This key prop is used to force the ProgressBar component to re-render */}
+    <ProgressBar key={currentActiveQuestion} timeout={10000} onTimeOut={handleSkipQuestion}  />
     <div id="questions">
         <h2>{QUESTIONS[currentActiveQuestion].text}</h2>
         <ul id="answers">
